c5734e6faedcd9977d6e8cf9d1879f757b40a0b8
AlexBaskin <solohinalex1@gmail.com>
2023-01-19 19:18:08 +0200

    Bereshit -- Begins --Add Adam and Eva --First Terrain

--- Assets/UIToolkit/Scripts/MarkInEditor.cs
+++ Assets/UIToolkit/Scripts/MarkInEditor.cs
@@ -1,34 +1,56 @@
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-[CustomEditor(typeof(SpawnShop))]
-public class MarkInEditor : Editor
-{
+[CustomEditor(typeof(CommentedUIEditor))]
+public class MarkInEditor : Editor {
     public VisualTreeAsset m_InspectorXML;
 
     private string oldText;
     private TextField _textField;
-    
+    private VisualElement root;
+
     public override VisualElement CreateInspectorGUI() {
         oldText = PlayerPrefs.GetString("text", oldText);
-        
+
         // Create a new VisualElement to be the root of our inspector UI
-        VisualElement root = new VisualElement();
+        root = new VisualElement();
 
         m_InspectorXML.CloneTree(root);
-        _textField = root.Q<TextField>("InputText");
+        root = root.Q<VisualElement>();
+
+        root.style.height = new StyleLength(100f);
         
+        _textField = root.Q<TextField>("InputText");
+
         _textField.RegisterValueChangedCallback(TextChange);
         _textField.SetValueWithoutNotify(oldText);
+        
+       ChangeTextFieldStyle(100f);
 
-        // Return the finished inspector UI
+       // Return the finished inspector UI
         return root;
+    }
+
+    private void ChangeTextFieldStyle(float offset) {
+        _textField.style.height = new StyleLength(( _textField.style.height.value.value + offset));
+        root.style.height = new StyleLength((root.style.height.value.value + offset));
+        root.style.color = Color.red;
     }
 
 
     private void TextChange(ChangeEvent<string> input) {
-        oldText = input.newValue;
+
+        var value = input.newValue;
+        value = input.newValue.Replace("<br>", "\n");
+        if (value.EndsWith("\n")) {
+            ChangeTextFieldStyle(20f);
+        }
+        oldText = value;
+        
+        // ChangeTextFieldStyle(10);
+        
         PlayerPrefs.SetString("text", oldText);
     }
 }
